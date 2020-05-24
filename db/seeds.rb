eac7cf08c7af68f788155e18005a2549ffdcc49e
Yaz <yasmineoshaugh@gmail.com>
2020-05-23 19:36:27 -0700

    seeds script added

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require "csv"
+
+
+CSV.foreach("db/works-seeds.csv", headers: true, header_converters: :symbol) do |row|
+  Work.create(
+    title: row[:title],
+    category: row[:category], 
+    creator: row[:creator],
+    published: row[:publication_year],
+    description: row[:description]
+  )
+end
+
+# category,title,creator,publication_year,description
+# (:title, :category, :creator, :published, :description)
