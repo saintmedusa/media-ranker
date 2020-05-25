6ad50c691d166088f4d5ec61d3d437590a61a8b7
Yaz <yasmineoshaugh@gmail.com>
2020-05-24 17:35:00 -0700

    fleshed out user controller actions index, show, new, and create - with corresponding routes in routes.rb. Also deleted extraneous user#actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,30 @@
 class UsersController < ApplicationController
   def index
+    @users = User.all
   end
 
   def show
-  end
-
-  def edit
-  end
-
-  def update
+    @user = User.find_by(id: params[:id])
   end
 
   def new
+    @user = User.new
   end
 
   def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+      return
+    else
+      render :new
+      return
+    end
+  end
+  
+  private
+  def user_params
+    return params.require(:user).permit(:username)
   end
 
-  def destroy
-  end
 end
