c80e7eff67702e7e684b32d14d489c150862e7de
Yaz <yasmineoshaugh@gmail.com>
2020-05-25 04:36:36 -0700

    added basic validations tests and one method test, created ew methods in user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,15 @@
+require_relative 'model_helper_methods'
 class User < ApplicationRecord
   validates :username, presence: true, uniqueness: true
+  def joined_date
+    return format_datetime(self.created_at)
+  end
+  def format_datetime(datetime)
+    datetime_string = datetime.to_s
+    time_array = datetime_string.split(/\D/)
+    time_array = time_array.map {|e| e.to_i }
+    time = Time.new(*time_array)
+    formatted_time_string = time.strftime("%D")#("%a, %D at %r")
+    return formatted_time_string
+  end
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,19 @@
 require "test_helper"
 
 describe User do
-  # it "does a thing" do
-  #   value(1+1).must_equal 2
-  # end
+  it "creates valid user" do
+    user = User.new(username: "name")
+    expect(user.valid?).must_equal true
+  end
+  it "rejects nil username" do
+    user = User.new(username: "")
+    expect(user.valid?).must_equal false
+  end
+  it "rejects duplicate user" do
+    user1 = User.new(username: "sarah")
+    user2 = User.new(username: "sarah")
+    expect(user2.valid?).must_equal false
+  end
+  it "returns joined date" do
+  end
 end
