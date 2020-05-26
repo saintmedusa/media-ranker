30fc2546249d2a663db78c2cd77ba68497eb7a38
Yaz <yasmineoshaugh@gmail.com>
2020-05-26 09:44:49 -0700

    fixed top-10 link bug

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "works#top_10"
   resources :works
-  get 'works/top', to: 'works#top_10', as: 'top_works'
   resources :users, only: [:index, :show, :new, :create]
 end
