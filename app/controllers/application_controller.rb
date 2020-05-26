8c63e1d72f45799ab0667e202f7330b0a09f7592
Yaz <yasmineoshaugh@gmail.com>
2020-05-26 09:53:51 -0700

    added user-auth controller filter in app_controller and added filter conditions to work_controller, user_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :require_login
+
+  def current_user
+    @current_user = User.find_by(id: session[:current_user_id])
+  end
+
+  def require_login
+    if current_user.nil?
+      flash[:error] = "You must be logged in to do that"
+      redirect_to login_path
+    end
+  end
 end
